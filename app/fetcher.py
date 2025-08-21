76f5984a514254994996eb963600cc6e9438c128
YosefGu <yos770yos@gmail.com>
2025-08-21 15:16:27 +0300

    updata fetcher to use env

--- app/fetcher.py
+++ app/fetcher.py
@@ -1,16 +1,21 @@
+import os
 from pymongo import MongoClient
+from dotenv import load_dotenv
 
+load_dotenv()
 
 class Fetcher():
 
-    def __init__(self, user, password, db):
-        self.client = MongoClient(f'mongodb+srv://{user}:{password}@{db}.gurutam.mongodb.net/')
-        self.db = db
+    def __init__(self):
+        self.user = os.getenv('MONGO_USER', 'IRGC')
+        self.password = os.getenv('MONGO_PASSWORD', 'iraniraniran')
+        self.db = os.getenv('MONGO_DB', 'IranMalDB')
+        self.client = MongoClient(f'mongodb+srv://{self.user}:{self.password}@{self.db}.gurutam.mongodb.net/')
+        
        
     def get_data(self):
-        collection = 'tweets'
-        return list(self.client[self.db][collection].find())
+        collection = self.client[self.db].list_collection_names()
+        data_list = list(self.client[self.db][collection[0]].find())
+        return data_list
     
-# if __name__ == '__main__':
-#     fetcher = Fetcher()
-#     fetcher.get_data()
+    
