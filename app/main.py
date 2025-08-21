12eb66348b52f0741ee0d3b167f2e28ddf4fd640
YosefGu <yos770yos@gmail.com>
2025-08-21 15:15:20 +0300

    creating a server

--- app/main.py
+++ app/main.py
@@ -1,9 +1,21 @@
 from fastapi import FastAPI
+from manager import Manager
+import uvicorn
 
+manager = Manager()
+manager.run()
 
 app = FastAPI()
 
+@app.get('/')
+def home():
+    return {"Active": "server is running!"}
 
 @app.get('/data')
 def get_data():
-    pass
+    result = manager.get_data_result()
+    return {"result": result}
+
+
+if __name__ == "__main__":
+    uvicorn.run(app)
